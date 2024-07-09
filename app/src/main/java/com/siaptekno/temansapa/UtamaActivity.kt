f9d333ddfe4f76ddcdcf532eca9fecd24138a9b4
e-haikal <taufanasman@yahoo.com>
2024-07-09 15:09:38 +0700

    home: add bottom navbar

--- app/src/main/java/com/siaptekno/temansapa/UtamaActivity.kt
+++ app/src/main/java/com/siaptekno/temansapa/UtamaActivity.kt
@@ -1,20 +1,21 @@
 package com.siaptekno.temansapa
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.get
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class UtamaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_utama)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.get(2).isEnabled = false
     }
 }
